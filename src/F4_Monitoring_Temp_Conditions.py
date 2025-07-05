5ba2986057046be47aeb98192ac80ec350782b9a
Nathan <nathanchew.24@ichat.sp.edu.sg>
2025-07-05 23:36:24 +0800

    f4 bug fix v2

--- src/F4_Monitoring_Temp_Conditions.py
+++ src/F4_Monitoring_Temp_Conditions.py
@@ -6,7 +6,6 @@ from threading import Thread
 import smtplib
 from email.message import EmailMessage
 
-global temp
 temp=0
 
 check10=0
@@ -26,7 +25,7 @@ def main():
     temp_check_thread.start()
     temp_Monitor_thread=Thread(target=temp_Monitor, daemon = True)
     temp_Monitor_thread.start()
-    ledBlink_thread=Thread(target=ledBlink,daemon=True)
+    ledBlink_thread=Thread(target=ledBlink)
     ledBlink_thread.start()
 
 def tempGet(): #constantly gets temp through thread in main
@@ -36,6 +35,7 @@ def tempGet(): #constantly gets temp through thread in main
         time.sleep(2) #prevent lag?
 
 def ledBlink():
+    global temp
     while True:
         if temp >= 20:
             led.set_output(24,10)
@@ -44,8 +44,9 @@ def ledBlink():
             time.sleep(0.2)
 
 def temp_Monitor():
-    global check10, check20, purchaseCheck, LCD
+    global check10, check20, purchaseCheck, LCD, temp
     waiting_for_payment = False
+    out_of_order = False
     while True:
         if temp>=10 and check10 == 0: #only emails staff
             msg = email_content(10)
@@ -66,15 +67,21 @@ def temp_Monitor():
             check20 = 1
             check10 = 1
 
-        else:
-            waiting_for_payment = False
+        if temp<10:
             check10=0
+
+        elif temp<20:
+            waiting_for_payment = False
             check20=0
 
-        if purchaseCheck == 0:
+        if purchaseCheck == 0 and not out_of_order:
                 LCD.lcd_clear()
                 LCD.lcd_display_string("Machine out", 1)
                 LCD.lcd_display_string("of order", 2)
+                out_of_order = True
+
+        elif purchaseCheck == 1:
+            out_of_order = False
 
         else:
             waiting_for_payment = True  #remove after implementing purchase check is implemented
