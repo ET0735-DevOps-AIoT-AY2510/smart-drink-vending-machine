297e17c1e72b2f9e3f16fd9b33e23e224757e2e2
IzzZayan <izzzayan.24@ichat.sp.edu.sg>
2025-07-06 16:47:48 +0800

    card payment v1

--- src/F2_card_payment.py
+++ src/F2_card_payment.py
@@ -20,7 +20,6 @@ drink_database = {
 global drink
 
 def main():
-   def main():
     global LCD, drink, last_key_time
 
     # initialize hardware
@@ -28,13 +27,13 @@ def main():
     LCD = LCD.lcd()
 
     #start threads
-    Thread(target=keypad.get_key, daemon=True).start()
+    Thread(target=key_input, daemon=True).start()
     Thread(target=check_inactivity, daemon=True).start()
     Thread(target=rfid_input, daemon=True).start()  #start RFID checking
 
 
 def tap_card_lcd_display(): 
-    LCD.lcd_clear 
+    LCD.lcd_clear() 
     LCD.lcd_display_string(f"{drink['name']} ${drink['price']}", 1)
     LCD.lcd_display_string("Tap card on reader", 2)
 
@@ -43,46 +42,53 @@ def check_inactivity():
     while True:
         if time.time() - last_key_time > 30: 
             homescreen() 
-            return 
+            last_key_time = time.time() #reset timer to avoid looping
+        time.sleep(5)
 
 def key_input(): 
-    global last_key_time
-    if not shared_keypad_queue.empty():
-        key = shared_keypad_queue.get()
-        last_key_time = time.time()
+    while True:
+        global last_key_time
+        if not shared_keypad_queue.empty(): #check if key is pressed 
+            key = shared_keypad_queue.get() #get the key 
+            last_key_time = time.time() 
 
-        if key == "*": 
-            LCD.lcd_clear()
-            LCD.lcd_display_string("Returning to", 1)
-            LCD.lcd_display_string("payment options", 2)
-            time.sleep(2)
-            LCD.lcd_clear()
-            LCD.lcd_display_string(f"{drink['name']} ${drink['price']}", 1)
-            LCD.lcd_display_string("1=Card 2=QR Code", 2)
-            return True 
-        
-    return False 
+            if key == "*": #for when they want to go back to payment options
+                LCD.lcd_clear()
+                LCD.lcd_display_string("Returning to", 1)
+                LCD.lcd_display_string("payment options", 2)
+                time.sleep(5) 
+                LCD.lcd_clear()
+                LCD.lcd_display_string(f"{drink['name']} ${drink['price']}", 1) 
+                LCD.lcd_display_string("1=Card 2=QR Code", 2)
+                return True 
+            
+        return False 
 
 def rfid_input():
     global last_key_time
-    card_data = rfid_reader.read_rfid_card() 
 
-    if card_data: #check if card was tapped
-        last_key_time = time.time() #update time to when card is tapped
+    while True: 
+        card_data = rfid_reader.read_rfid_card() 
 
-        if card_data = 
-            LCD.lcd_clear()
-            LCD.lcd_display_string("Payment Success", 1)
-            time.sleep(2)
-            tap_card_lcd_display()
-        else:
-            LCD.lcd_clear()
-            LCD.lcd_display_string("Card declined,", 1)
-            LCD.lcd_display_string("please try again", 2)
-            time.sleep(3)
-            tap_card_lcd_display()
-        
-    return False
+        if card_data: #check if card was tapped
+            last_key_time = time.time() #update time to when card is tapped
+
+            accepted_card_data = ["1098490313"] #placeholder for id 
+
+            if card_data in accepted_card_data :  #accepted card
+                LCD.lcd_clear()
+                LCD.lcd_display_string("Payment Success", 1)
+                time.sleep(2)
+                tap_card_lcd_display()
+            else:
+                LCD.lcd_clear() #declined card
+                LCD.lcd_display_string("Card declined,", 1)
+                LCD.lcd_display_string("please try again", 2)
+                time.sleep(3)
+                tap_card_lcd_display()
+        time.sleep(5)
+            
+        return False
 
 if __name__ == "__main__":
     main()
