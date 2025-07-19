40e9134160c3796b6b8c121f5a0e77a45a7a42e1
Terence <terencetng.24@ichat.sp.edu.sg>
2025-07-19 16:30:13 +0800

    Initialised camera in variables.py and removed picam2.close() to reduce camera capture delay in F8

--- src/F8_burglar_detection.py
+++ src/F8_burglar_detection.py
@@ -46,16 +46,7 @@ def main():
 
 
 def camerafeature():
-    picam2 = Picamera2()
-    camera_config = picam2.create_still_configuration(main={"size": (1920,
-                                                                     1080)}, lores={"size": (640, 480)}, display="lores")
-    picam2.configure(camera_config)
-    picam2.start_preview(Preview.QTGL)
-    picam2.start()
-    time.sleep(2)
-    picam2.capture_file("idk.jpg")
-    picam2.stop_preview()
-    picam2.close()
+    g.picam2.capture_file("idk.jpg")
     g.send_email(
         receiver_email='terencetngkc2007@gmail.com',
         subject='Image of Burglar',
--- src/variables.py
+++ src/variables.py
@@ -8,7 +8,14 @@ import smtplib
 from email.message import EmailMessage
 from email.utils import make_msgid
 from pathlib import Path
+from picamera2 import Picamera2, Preview
 
+picam2 = Picamera2()
+camera_config = picam2.create_still_configuration(main={"size": (1920,
+                                                                 1080)}, lores={"size": (640, 480)}, display="lores")
+picam2.configure(camera_config)
+# picam2.start_preview(Preview.QTGL)
+picam2.start()
 card_declined = True
 selection = 0
 
